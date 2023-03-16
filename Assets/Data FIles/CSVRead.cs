8430afa4b609d619a5a4a63940f7173711b25ee9
jacint <jacint122001@gmail.com>
2023-03-16 17:42:48 +0000

    only even rows saved

--- Assets/Data FIles/CSVRead.cs
+++ Assets/Data FIles/CSVRead.cs
@@ -19,15 +19,15 @@ public class CSVRead : MonoBehaviour
     public Points[] ReadCSV()
     {
         string[] elements = csvFile.text.Split(new string [] {",",",","\n"}, StringSplitOptions.None);
-        int size = elements.Length/4;
+        int size = elements.Length/8;
         Points[] data_set  = new Points[size];
 
         for (int j = 0; j < size; j++)
         {
             data_set[j] = new Points();
-            data_set[j].x = float.Parse(elements[4 * (j)]);
-            data_set[j].y = float.Parse(elements[4 * (j) + 1 ]);
-            data_set[j].v = float.Parse(elements[4 * (j) + 3 ]);
+            data_set[j].x = float.Parse(elements[8 * (j) + 4]);
+            data_set[j].y = float.Parse(elements[8 * (j) + 5 ]);
+            data_set[j].v = float.Parse(elements[8 * (j) + 7 ]);
         }
 
         return data_set;
