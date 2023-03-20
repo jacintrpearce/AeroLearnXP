5b474f89c3a2331c6e557e6ceb8573634d2d6c2a
jacint <jacint122001@gmail.com>
2023-03-20 18:21:56 +0000

    new buttons

--- Assets/Menu Panel/AL_MainMenu.cs
+++ Assets/Menu Panel/AL_MainMenu.cs
@@ -6,6 +6,7 @@ public class AL_MainMenu : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject AerofoilMenu;
+    public GameObject RAE2822Menu;
 
     // Start is called before the first frame update
     void Start()
@@ -13,24 +14,40 @@ public class AL_MainMenu : MonoBehaviour
         MainMenuButton();
     }
 
-    public void PlayButton()
+    public void CfButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("RAE2822 Level");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Skin Friction Scene");
     }
+    public void VButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Velocity Scene");
+    }
+    public void PButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Pressure Scene");
+    }
+    public void RAE2822MenuButton()
+    {
+        MainMenu.SetActive(false);
+        AerofoilMenu.SetActive(false);
+        RAE2822Menu.SetActive(true);
 
+    }
     public void AerofoilMenuButton()
     {
-        // Show Credits Menu
         MainMenu.SetActive(false);
         AerofoilMenu.SetActive(true);
+        RAE2822Menu.SetActive(false);
     }
 
     public void MainMenuButton()
     {
-        // Show Main Menu
         MainMenu.SetActive(true);
         AerofoilMenu.SetActive(false);
+        RAE2822Menu.SetActive(false);
     }
 
     public void QuitButton()
