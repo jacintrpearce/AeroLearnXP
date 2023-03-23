0ac5ddf33a3dfb5393488464cab7125c17d2691b
jacint <jacint122001@gmail.com>
2023-03-23 18:13:59 +0000

    test

--- Assets/Solution Visualisation/Pressure/CSVReadCp.cs
+++ Assets/Solution Visualisation/Pressure/CSVReadCp.cs
@@ -20,22 +20,34 @@ public class CSVReadCp : MonoBehaviour
         int size = elements.Length/4;
         PointsCp[] data_setCp  = new PointsCp[size];
 
+        int filteredPointsCount = 0; // initialize counter variable to 0
+
         for (int j = 0; j < size; j++)
         {
-            data_setCp[j] = new PointsCp();
             float x = float.Parse(elements[4 * (j)]);
             float y = float.Parse(elements[4 * (j) + 1]);
             float v = float.Parse(elements[4 * (j) + 3]);
             
             if ((x < 0 && x > -0.3f && Mathf.Abs(y) < 1) || (x >= 0 && x < 1f) && Mathf.Abs(y) < 1)
             {
-                data_setCp[j].x = x;
-                data_setCp[j].y = y;
-                data_setCp[j].v = v;
-            }
+                data_setCp[filteredPointsCount] = new PointsCp(); // create new instance of PointsCp
+                data_setCp[filteredPointsCount].x = x;
+                data_setCp[filteredPointsCount].y = y;
+                data_setCp[filteredPointsCount].v = v;
 
+                filteredPointsCount++; // increment counter variable for each filtered point
+            }
         }
 
+        PointsCp[] filteredData_setCp = new PointsCp[filteredPointsCount]; // create new array with size of filteredPointsCount
+
+        for (int i = 0; i < filteredPointsCount; i++)
+        {
+            filteredData_setCp[i] = new PointsCp(); // create new instance of PointsCp
+            filteredData_setCp[i].x = data_setCp[i].x;
+            filteredData_setCp[i].y = data_setCp[i].y;
+            filteredData_setCp[i].v = data_setCp[i].v;
+        }
         return data_setCp;
     }
     
