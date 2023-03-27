bfe33a7043512dec2f22d55b0e3d35f72837a534
jacint <jacint122001@gmail.com>
2023-03-27 03:23:10 +0100

    color scale change

--- Assets/Solution Visualisation/Pressure/CSVReadCp.cs
+++ Assets/Solution Visualisation/Pressure/CSVReadCp.cs
@@ -1,5 +1,8 @@
 using System;
+<<<<<<< HEAD
 using System.Collections.Generic;
+=======
+>>>>>>> origin/master
 using UnityEngine;
 
 public class CSVReadCp : MonoBehaviour
@@ -14,6 +17,7 @@ public class CSVReadCp : MonoBehaviour
         public float y;
         public float v;
     }
+<<<<<<< HEAD
     public PointsCp[] ReadCSV()
         {
             string[] elements = csvFile.text.Split(new string [] {",",",","\n"}, StringSplitOptions.None);
@@ -42,8 +46,40 @@ public class CSVReadCp : MonoBehaviour
             // convert the list to an array and return it
             return data_setCp .ToArray();
         }
+=======
+
+    public PointsCp[] ReadCSV()
+    {
+        string[] elements = csvFile.text.Split(new string [] {",",",","\n"}, StringSplitOptions.None);
+        int size = elements.Length/4;
+        PointsCp[] data_setCp  = new PointsCp[size];
+
+        for (int j = 0; j < size; j++)
+        {
+            data_setCp[j] = new PointsCp();
+            float x = float.Parse(elements[4 * (j)]);
+            float y = float.Parse(elements[4 * (j) + 1]);
+            float v = float.Parse(elements[4 * (j) + 3]);
+            
+            if ((x < 0 && x > -0.3f && Mathf.Abs(y) < 1) || (x >= 0 && x < 1f) && Mathf.Abs(y) < 1)
+            {
+                data_setCp[j].x = x;
+                data_setCp[j].y = y;
+                data_setCp[j].v = v;
+            }
+
+        }
+
+        return data_setCp;
+    }
+    
+>>>>>>> origin/master
     void Start()
     {
         ReadCSV();
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> origin/master
