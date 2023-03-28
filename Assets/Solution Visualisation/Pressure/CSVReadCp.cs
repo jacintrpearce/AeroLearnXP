1aaf3f2b98dc3e89c2679c01abba405eb1fb538a
jacint <jacint122001@gmail.com>
2023-03-28 18:07:23 +0100

    row number filter

--- Assets/Solution Visualisation/Pressure/CSVReadCp.cs
+++ Assets/Solution Visualisation/Pressure/CSVReadCp.cs
@@ -21,27 +21,35 @@ public class CSVReadCp : MonoBehaviour
 
             // initialize an empty list to store the valid points
             List<PointsCp> data_setCp = new List<PointsCp>();
-            
+            var rowCounter = 0;
+
             for (int j = 0; j < elements.Length / 4; j++)
             {
                 float x = float.Parse(elements[4 * j]);
                 float y = float.Parse(elements[4 * j + 1]);
                 float v = float.Parse(elements[4 * j + 3]);
 
-                if ((x < 0 && x > -0.2f && Mathf.Abs(y) < 0.08) || (x >= 0 && x < 1f && Mathf.Abs(y) < 0.08))
+                rowCounter++;
+
+                if (((x < 0 && x > -0.2f && Mathf.Abs(y) < 0.08) || (x >= 0 && x < 1f && Mathf.Abs(y) < 0.08)))
                 {
-                    PointsCp point = new PointsCp();
-                    point.x = x;
-                    point.y = y;
-                    point.v = v;
-                    
-                    // add the point to the list
-                    data_setCp .Add(point);
+                    // Only save values corresponding to N number of rows (even (N = 2), every 3 (N = 3) etc)
+                    if (rowCounter % 2 == 0)
+                    {
+                        PointsCp point = new PointsCp();
+                        point.x = x;
+                        point.y = y;
+                        point.v = v;
+
+                        // add the point to the list
+                        data_setCp.Add(point);
+                    }
                 }
             }
-            
+
             // convert the list to an array and return it
-            return data_setCp .ToArray();
+            return data_setCp.ToArray();
+
         }
     void Start()
     {
