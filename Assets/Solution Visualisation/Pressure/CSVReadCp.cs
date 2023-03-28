03966c9de328d703993c55cb49a291e210b70e50
jacint <jacint122001@gmail.com>
2023-03-28 18:43:09 +0100

    greater control upstream and downstream

--- Assets/Solution Visualisation/Pressure/CSVReadCp.cs
+++ Assets/Solution Visualisation/Pressure/CSVReadCp.cs
@@ -30,8 +30,9 @@ public class CSVReadCp : MonoBehaviour
                 float v = float.Parse(elements[4 * j + 3]);
 
                 rowCounter++;
-
-                if (((x < 0 && x > -0.2f) || (x >= 0 && x < 1f)))
+                
+                // Check the values to be saved starting from aerofoil in the x direction
+                if (x >= 0 && x < 1f)
                 {
                     // Only save values corresponding to N number of rows (even (N = 2), every 3 (N = 3) etc)
                     if (rowCounter % 2 == 0 && Mathf.Abs(y) > 0.05 && Mathf.Abs(y) < 0.15)
@@ -45,6 +46,20 @@ public class CSVReadCp : MonoBehaviour
                     } 
                     // Data closer to aerofoil
                     else if (rowCounter % 2 == 0 && Mathf.Abs(y) > 0.05 && Mathf.Abs(y) < 0.15)
+                    {
+                        PointsCp point = new PointsCp();
+                        point.x = x;
+                        point.y = y;
+                        point.v = v;
+                        // add the point to the list
+                        data_setCp.Add(point);
+                    }
+                }
+                // Data upstream and downstream of aerofoil
+                else if ((x < 0 && x > -0.2f) || (x > 1 && x < 1.2f))
+                {
+                    // Only save values corresponding to N number of rows (even (N = 2), every 3 (N = 3) etc)
+                    if (rowCounter % 2 == 0)
                     {
                         PointsCp point = new PointsCp();
                         point.x = x;
