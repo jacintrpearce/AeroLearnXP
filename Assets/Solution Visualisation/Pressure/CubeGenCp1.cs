863f533c1704f542b24653adbfd41400194fe77c
jacint <jacint122001@gmail.com>
2023-03-29 16:48:43 +0100

    additional colours

--- Assets/Solution Visualisation/Pressure/CubeGenCp1.cs
+++ Assets/Solution Visualisation/Pressure/CubeGenCp1.cs
@@ -16,20 +16,21 @@ public class CubeGenCp1 : MonoBehaviour
         float maxV = float.MinValue;
         float minV = float.MaxValue;
         
-        var colors = new List<Color>();
-        int numColors = 20;
-        // Add first color (blue)
-        colors.Add(Color.blue);
-
-        // Add 18 intermediate colors between blue and red
-        for (int i = 1; i < 19; i++)
+        var colors = new[]
         {
-            Color color = Color.Lerp(Color.blue, Color.red, (float)i / (numColors - 1));
-            colors.Add(color);
-        }
+            Color.blue, 
+            Color.Lerp(Color.blue, new Color(0.5f, 0.75f, 1.0f), 0.25f), 
+            new Color(0.5f, 0.75f, 1.0f),
+            Color.Lerp(new Color(0.5f, 0.75f, 1.0f), Color.green, 0.25f), 
+            Color.green, 
+            Color.Lerp(Color.green, Color.yellow, 0.25f),
+            Color.yellow, 
+            Color.Lerp(Color.yellow, new Color(1.0f, 0.75f, 0f), 0.25f), 
+            new Color(1.0f, 0.75f, 0f),
+            Color.Lerp(new Color(1.0f, 0.75f, 0f), Color.red, 0.25f), 
+            Color.red
+        };
 
-        // Add last color (red)
-        colors.Add(Color.red);
         for (int i = 0; i < data_setCp.Length; i++)
         {
             float v = data_setCp[i].v;
@@ -50,20 +51,20 @@ public class CubeGenCp1 : MonoBehaviour
             cube.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
             Renderer renderer = cube.GetComponent<Renderer>();
 
-            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp((data_setCp[i].v - minV) / (maxV - minV), 0f, 1f) * (numColors - 1));
-            Color color = colors[Mathf.Clamp(scaleIndex, 0, numColors - 1)];
+            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp((data_setCp[i].v - minV) / (maxV - minV), 0f, 1f) * (colors.Length - 1));
+            Color color = colors[Mathf.Clamp(scaleIndex, 0, colors.Length - 1)];
 
             Material material = new Material(Shader.Find("Standard"));
             material.color = color;
             renderer.material = material;
         }
-        // Update Legend Max and Min values
-        GameObject maxObject = GameObject.Find("Max 1");
-        TextMeshPro maxText = maxObject.GetComponent<TextMeshPro>();
-        maxText.text = "Max: " + maxV.ToString("0.##E+00");
+       // Update Legend Max and Min values
+       GameObject maxObject = GameObject.Find("Max 1");
+       TextMeshPro maxText = maxObject.GetComponent<TextMeshPro>();
+       maxText.text = "Max: " + maxV.ToString("0.##E+00");
        
-        GameObject minObject = GameObject.Find("Min 1");
-        TextMeshPro minText = minObject.GetComponent<TextMeshPro>();
-        minText.text = "Min: " + minV.ToString("0.##E+00");
+       GameObject minObject = GameObject.Find("Min 1");
+       TextMeshPro minText = minObject.GetComponent<TextMeshPro>();
+       minText.text = "Min: " + minV.ToString("0.##E+00");
     }
 }
