7834a241f96ffdd05bec8f14c616a78b3328272d
jacint <jacint122001@gmail.com>
2023-03-29 18:06:39 +0100

    better min max def + angle of attack transform

--- Assets/Solution Visualisation/Skin Friction/CubeGenCf1.cs
+++ Assets/Solution Visualisation/Skin Friction/CubeGenCf1.cs
@@ -1,38 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class CubeGenCf1 : MonoBehaviour
 {
     public CSVReadCF csvRead;
-    public float scaleX = 0.01f;
-    public float scaleY = 0.01f;
-    public float scaleZ = 0.01f;
+    public float scaleX = 0.007f;
+    public float scaleY = 0.007f;
+    public float scaleZ = 0.007f;
+
     void Start()
     {
         CSVReadCF.Points[] data_set = csvRead.ReadCSV();
         int index = data_set.Length;
+        float maxV = float.MinValue;
+        float minV = float.MaxValue;
         
+        // Extract angle of attack of aerofoil based on the Z rotation angle in degrees
+        float aoa = transform.parent.rotation.eulerAngles.z;
+        float aoaRad = aoa * Mathf.Deg2Rad;
+        float sinAoA = Mathf.Sin(aoaRad);
+        float cosAoA = Mathf.Cos(aoaRad);
+
         //Colour array
         var colors = new[] { Color.blue, Color.Lerp(Color.blue, Color.cyan, 0.25f), Color.cyan, Color.Lerp(Color.cyan, Color.green, 0.25f), Color.green, Color.Lerp(Color.green, Color.yellow, 0.25f), Color.yellow, Color.Lerp(Color.yellow, new Color(1.0f, 0.5f, 0f), 0.25f), new Color(1.0f, 0.5f, 0f), Color.Lerp(new Color(1.0f, 0.5f, 0f), Color.red, 0.25f), Color.red };
         
         for (int i = 0; i < index; i++)
         {
+            float v = data_set[i].v;
+
+            if (v > maxV)
+            {
+                maxV = v;
+            }
+
+            if (v < minV)
+            {
+                minV = v;
+            }
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
             cube.transform.parent = transform;
             Vector3 parentPos = transform.position;
-            cube.transform.position = new Vector3(parentPos.x + data_set[i].x, parentPos.y + data_set[i].y, -parentPos.z);
+            
+            float locX = parentPos.x + ((data_set[i].x * cosAoA) - (data_set[i].y * sinAoA));
+            float locY = parentPos.y + ((data_set[i].x * sinAoA) + (data_set[i].y * cosAoA));
+            cube.transform.position = new Vector3(locX, locY , -parentPos.z);
             cube.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
             Renderer renderer = cube.GetComponent<Renderer>();
-            
-            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp(data_set[i].v, 0.0000661f, 0.000579f) / 0.000579f * (colors.Length - 1));
+    
+            // Specify min and max CF values
+            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp(data_set[i].v, 0.0000182993f, 0.0014567246f) / 0.0014567246f * (colors.Length - 1));
             Color color = colors[scaleIndex];
-            
+
             Material material = new Material(Shader.Find("Standard"));
             material.color = color;
             renderer.material = material;
-            
+
         }
+        // Update Legend Max and Min values
+        GameObject maxObject = GameObject.Find("Max 2");
+        TextMeshPro maxText = maxObject.GetComponent<TextMeshPro>();
+        maxText.text = "Max: " + maxV.ToString("0.##E+00");
+       
+        GameObject minObject = GameObject.Find("Min 2");
+        TextMeshPro minText = minObject.GetComponent<TextMeshPro>();
+        minText.text = "Min: " + minV.ToString("0.##E+00");
     }
 }
-
--- Assets/Solution Visualisation/Skin Friction/CubeGenCf4.cs
+++ Assets/Solution Visualisation/Skin Friction/CubeGenCf4.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class CubeGenCf4 : MonoBehaviour
@@ -8,39 +8,63 @@ public class CubeGenCf4 : MonoBehaviour
     public float scaleX = 0.007f;
     public float scaleY = 0.007f;
     public float scaleZ = 0.007f;
+
     void Start()
     {
         CSVReadCF.Points[] data_set = csvRead.ReadCSV();
         int index = data_set.Length;
+        float maxV = float.MinValue;
+        float minV = float.MaxValue;
         
+        // Extract angle of attack of aerofoil based on the Z rotation angle in degrees
+        float aoa = transform.parent.rotation.eulerAngles.z;
+        float aoaRad = aoa * Mathf.Deg2Rad;
+        float sinAoA = Mathf.Sin(aoaRad);
+        float cosAoA = Mathf.Cos(aoaRad);
+
         //Colour array
         var colors = new[] { Color.blue, Color.Lerp(Color.blue, Color.cyan, 0.25f), Color.cyan, Color.Lerp(Color.cyan, Color.green, 0.25f), Color.green, Color.Lerp(Color.green, Color.yellow, 0.25f), Color.yellow, Color.Lerp(Color.yellow, new Color(1.0f, 0.5f, 0f), 0.25f), new Color(1.0f, 0.5f, 0f), Color.Lerp(new Color(1.0f, 0.5f, 0f), Color.red, 0.25f), Color.red };
         
         for (int i = 0; i < index; i++)
         {
+            float v = data_set[i].v;
+
+            if (v > maxV)
+            {
+                maxV = v;
+            }
+
+            if (v < minV)
+            {
+                minV = v;
+            }
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            
-            //Set x and y coordinate with change in AoA = 14 degrees Refer to ChatGPT
-            // cos -14 = 0.970295726f
-            // sin -14 = -0.241921896f
+
             cube.transform.parent = transform;
             Vector3 parentPos = transform.position;
-            float locX = parentPos.x + ((data_set[i].x * 0.970295726f) - (data_set[i].y * -0.241921896f));
-            float locY = parentPos.y + ((data_set[i].x * -0.241921896f) + (data_set[i].y * 0.970295726f));
+            
+            float locX = parentPos.x + ((data_set[i].x * cosAoA) - (data_set[i].y * sinAoA));
+            float locY = parentPos.y + ((data_set[i].x * sinAoA) + (data_set[i].y * cosAoA));
             cube.transform.position = new Vector3(locX, locY , -parentPos.z);
             cube.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
             Renderer renderer = cube.GetComponent<Renderer>();
-            
+    
             // Specify min and max CF values
-            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp(data_set[i].v, 8.41e-07f, 6.06e-03f) / 6.06e-03f * (colors.Length - 1));
+            int scaleIndex = Mathf.RoundToInt(Mathf.Clamp(data_set[i].v, 0.0000182993f, 0.0014567246f) / 0.0014567246f * (colors.Length - 1));
             Color color = colors[scaleIndex];
-            
+
             Material material = new Material(Shader.Find("Standard"));
             material.color = color;
             renderer.material = material;
-            
+
         }
+        // Update Legend Max and Min values
+        GameObject maxObject = GameObject.Find("Max 2");
+        TextMeshPro maxText = maxObject.GetComponent<TextMeshPro>();
+        maxText.text = "Max: " + maxV.ToString("0.##E+00");
+       
+        GameObject minObject = GameObject.Find("Min 2");
+        TextMeshPro minText = minObject.GetComponent<TextMeshPro>();
+        minText.text = "Min: " + minV.ToString("0.##E+00");
     }
 }
-
-
