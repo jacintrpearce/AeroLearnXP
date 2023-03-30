df16db5427d45b24e87bfbfae1c6a6862534067f
jacint <jacint122001@gmail.com>
2023-03-30 02:07:05 +0100

    colour grad

---
+++ Assets/Solution Visualisation/LegendFiles/ColourGradient.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ColourGradient : MonoBehaviour
+{
+    void Start()
+    {
+        Gradient gradient = new Gradient();
+        // Add color keys and alpha keys to the gradient
+        GradientColorKey[] colorKeys = new GradientColorKey[2];
+        colorKeys[0].color = Color.red;
+        colorKeys[0].time = 0.0f;
+        colorKeys[1].color = Color.blue;
+        colorKeys[1].time = 1.0f;
+        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
+        alphaKeys[0].alpha = 1.0f;
+        alphaKeys[0].time = 0.0f;
+        alphaKeys[1].alpha = 1.0f;
+        alphaKeys[1].time = 1.0f;
+        gradient.SetKeys(colorKeys, alphaKeys);
+    }
+}
